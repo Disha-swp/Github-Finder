2c63e1b284f0ce29a35c28a96139dc1475d164ff
dishaSwarup <swarupdisha5@gmail.com>
2021-08-31 19:30:13 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from 'react';
+ import './App.css';
+import { Button,Card, Navbar,Container } from 'react-bootstrap'
 
 function App() {
+
+const [name, setName] = useState('');
+const [username, setUsername] = useState('');
+const [followers, setFollowers] = useState('');
+const [following, setFollowing] = useState('');
+const [repository, setRepository] = useState('');
+const [avatar , setAvatar] = useState([]);
+const [userInput, setUserInput] = useState(''); // for search
+const [error, setError] = useState(null);
+
+useEffect(() => {
+  // effect
+  fetch(' https://api.github.com/users/${userInput}')
+    .then(res => res.json())
+    .then(value => {
+      // console.log(value);
+      setData(value);
+    })
+  }, []);
+  const handleSearch = (e) => {
+    setUserInput(e.target.value);
+  }
+  const setData = ({name,  //values fetched from api
+     login, 
+     followers,
+     following,
+     public_repos,
+     avatar_url
+    }) => {
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepository(public_repos);
+    setAvatar(avatar_url);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand >GitHub User Finder</Navbar.Brand>
+    </Container>
+    </Navbar>
+      <div className="searchBlock">
+        <h3>Enter Username</h3>
+        <input type = "text" id="searchUser" placeholder="Username" onChange ={handleSearch}/> 
+        <Button variant="success" type = "submit"> Search</Button>
+      </div>
+      <div className="card">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={avatar} />
+        <Card.Body>
+          <Card.Title>{username}</Card.Title>
+          <Card.Text>{username}</Card.Text>
+          <Card.Text>Followers: {followers}</Card.Text>
+          <Card.Text>repos: {repository}</Card.Text>
+          <Card.Text>Following: {following}</Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      </div>
     </div>
+    
   );
 }
 
